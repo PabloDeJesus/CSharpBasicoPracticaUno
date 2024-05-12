9cfbae0e5b6ad1e39b7ee877e0e1bde0013cf33a
PabloDeJesus <161788354+PabloDeJesus@users.noreply.github.com>
2024-05-11 20:48:51 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Leer un número entero de tres dígitos y determinar si el primer dígito es igual al ultimo
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero de tres dígitos:");
+        int numero = int.Parse(Console.ReadLine());
+
+         
+        if (numero >= 100 && numero <= 999)
+        {
+            int primerDigito = numero / 100;  
+            int tercerDigito = numero % 10;  
+
+             
+            if (primerDigito == tercerDigito)
+            {
+                Console.WriteLine("El primer dígito es igual al último.");
+            }
+            else
+            {
+                Console.WriteLine("El primer dígito no es igual al último.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado no tiene tres dígitos.");
+        }
+    }
+}
