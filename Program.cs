55aea6059b8f7a4e40a8bf369f1876c055f6d41d
PabloDeJesus <161788354+PabloDeJesus@users.noreply.github.com>
2024-05-11 20:58:40 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Leer un número entero de tres dígitos y determinar cuántos dígitos pares tiene.
+
+        Console.WriteLine("Ingrese un número entero de tres dígitos:");
+        int numero = int.Parse(Console.ReadLine());
+
+        
+        if (numero >= 100 && numero <= 999)
+        {
+            int digito1 = numero / 100; 
+            int digito2 = (numero / 10) % 10;  
+            int digito3 = numero % 10;  
+
+            int contadorPares = 0;
+
+          
+            if (digito1 % 2 == 0)
+            {
+                contadorPares++;
+            }
+            if (digito2 % 2 == 0)
+            {
+                contadorPares++;
+            }
+            if (digito3 % 2 == 0)
+            {
+                contadorPares++;
+            }
+
+            Console.WriteLine($"El número de dígitos pares es: {contadorPares}");
+        }
+        else
+        {
+            Console.WriteLine("El número ingresado no tiene tres dígitos.");
+        }
+    }
+}
