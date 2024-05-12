852e31937b3e0b28eebf37710c47b32c672494b1
PabloDeJesus <161788354+PabloDeJesus@users.noreply.github.com>
2024-05-11 20:35:50 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Leer tres números enteros y determinar cuál es el mayor. Usar solamente dos variables.
+
+
+    
+        Console.WriteLine("Ingrese el primer número entero:");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número entero:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el tercer número entero:");
+        int numero3 = int.Parse(Console.ReadLine());
+
+        int maximo = numero1;  
+
+         
+        if (numero2 > maximo)
+        {
+            maximo = numero2;
+        }
+
+        
+        if (numero3 > maximo)
+        {
+            maximo = numero3;
+        }
+
+        Console.WriteLine($"El número máximo es: {maximo}");
+    
